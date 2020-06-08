b67c7ad55e5621cfa1dfadd057f6ad438388d55e
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-06-08 15:31:47 +0300

    Added messagebox exercise

---
+++ messagebox.py
@@ -0,0 +1,21 @@
+
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk() # Init app
+root.title("Messagebox") # Set title here
+root.iconbitmap("Images/neon.ico") # Insert icon here
+
+# showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
+
+def popup():
+    response = messagebox.askyesno("This is popup", "Hello World!")
+    if response == 1:
+        Label(root, text="You clicked Yes!").pack()
+    else:
+        Label(root, text="You clicked No!").pack()
+
+Button(root, text="Pop-up", command=popup).pack()
+
+
+root.mainloop() # Keep the app running
