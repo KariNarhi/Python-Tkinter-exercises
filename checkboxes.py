adec6e5e8add5b8b4d68886b8aa430a05bdc279d
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-06-08 18:58:38 +0300

    Added checkbox exercise

---
+++ checkboxes.py
@@ -0,0 +1,22 @@
+
+from tkinter import *
+
+root = Tk() # Init app
+root.title("Messagebox") # Set title here
+root.iconbitmap("Images/neon.ico") # Insert icon here
+root.geometry("300x300")
+
+def show():
+    myLabel = Label(root, text=var.get()).pack() # Show selected value
+
+var = StringVar()
+
+c = Checkbutton(root, text="Check this box", variable=var, onvalue="On", offvalue="Off") # Define checkbox
+c.deselect() # Initialize as non-selected
+c.pack()
+
+
+myButton = Button(root, text="Show selection", command=show).pack() # Button for showing selected value
+
+
+root.mainloop() # Keep the app running
