7bce4f4dc63d0f624ca8585216604f5933afab39
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-06-16 21:58:36 +0300

    Delete functionality & demo data changes

--- Tkinter-with-database/database.py
+++ Tkinter-with-database/database.py
@@ -28,7 +28,7 @@ c = conn.cursor()
 
 # Create text boxes
 f_name = Entry(root, width=30)
-f_name.grid(row=0, column=1, padx=20)
+f_name.grid(row=0, column=1, padx=20, pady=(10, 0))
 
 l_name = Entry(root, width=30)
 l_name.grid(row=1, column=1)
@@ -45,9 +45,12 @@ state.grid(row=4, column=1)
 zipcode = Entry(root, width=30)
 zipcode.grid(row=5, column=1)
 
+delete_box = Entry(root, width=30)
+delete_box.grid(row=9, column=1, pady=5)
+
 # Text box labels
 f_name_label = Label(root, text="First Name")
-f_name_label.grid(row=0, column=0)
+f_name_label.grid(row=0, column=0, pady=(10, 0))
 
 l_name_label = Label(root, text="Last Name")
 l_name_label.grid(row=1, column=0)
@@ -63,6 +66,9 @@ state_label.grid(row=4, column=0)
 
 zipcode_label = Label(root, text="Zipcode")
 zipcode_label.grid(row=5, column=0)
+
+delete_box_label = Label(root, text="ID to delete")
+delete_box_label.grid(row=9, column=0, pady=5)
 
 # Submit function
 def submit():
@@ -100,8 +106,11 @@ def submit():
 submit_btn = Button(root, text="Add record to database", command=submit)
 submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
 
+query_label = None
+
 # Query function
 def query():
+
     # Connect to database
     conn = sqlite3.connect('address_book.db')
     # Create connection cursor
@@ -109,15 +118,17 @@ def query():
 
     c.execute("SELECT *, oid FROM addresses")
     records = c.fetchall()
-    print(records)
+    # print(records)
 
     # Loop results
-    print_records = "" 
+    print_records = "  Name \t\t ID \n\n" 
     for record in records:
-        print_records += str(record) + "\n"
+        print_records += str(record[0]) + " " + str(record[1]) + "\t" + str(record[6]) + "\n"
+        print("\n" + str(record))
 
+    global query_label
     query_label = Label(root, text=print_records)
-    query_label.grid(row=8, column=0, columnspan=2)
+    query_label.grid(row=11, column=0, columnspan=2)
 
     # Commit changes
     conn.commit()
@@ -127,6 +138,30 @@ def query():
 # Query button
 query_btn = Button(root, text="Get records", command=query)
 query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=130)
+
+# Delete function
+def delete():
+    # Connect to database
+    conn = sqlite3.connect('address_book.db')
+    # Create connection cursor
+    c = conn.cursor()
+
+    c.execute("DELETE FROM addresses WHERE oid=" + delete_box.get())
+
+    # Commit changes
+    conn.commit()
+    # Close connection
+    conn.close()
+
+    # Clear Delete ID entry
+    delete_box.delete(0, END)
+
+    # Clear query results label
+    query_label.grid_forget()
+
+# Delete button
+delete_btn = Button(root, text="Delete record", command=delete)
+delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=125)
 
 # Commit changes
 conn.commit()
